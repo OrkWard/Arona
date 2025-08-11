eaf9e749eccbd873386c38d900c6b123afe221f0
orkward <orkward_o@outlook.com>
2025-08-12 00:12:36 +0800

    remove backup

--- apps/backup/prisma/migrations/20250127164601_add_invite/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Invite" (
-    "userId" TEXT NOT NULL,
-    "sourceGroupId" TEXT NOT NULL,
-    "targetGroupId" TEXT NOT NULL,
-
-    PRIMARY KEY ("userId", "sourceGroupId", "targetGroupId")
-);
